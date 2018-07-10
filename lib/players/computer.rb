76e9ce96779c7970acf127f385ec359bbe0f0f71
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-10 13:27:17 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,14 +4,10 @@ module Players
     def move(board)
       if board.cells[4] != "X" && board.cells[4] != "O"
         5
-      elsif board.cells[0] != "X" && board.cells[0] != "O"
-        1 
-      elsif board.cells[8] != "X" && board.cells[8] != "O"
-        9 
-      elsif board.cells[2] != "X" && board.cells[2] != "O"
-        3
-      elsif board.cells[6] != "X" && board.cells[6] != "O"
-        7
+      elsif board.cells[0] != "X" && board.cells[0] != "O" && board.cells[8] != "X" && board.cells[8] != "O"
+        small_array = ["1", "9"] 
+      elsif board.cells[2] != "X" && board.cells[2] != "O" && board.cells[6] != "X" && board.cells[6] != "O"
+        other_array = ["3", "6"]
       else 
         array = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
         array.sample 
