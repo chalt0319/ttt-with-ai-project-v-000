48f395f58ba0109a1b2b821d04067e49741c83ac
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-10 13:27:34 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,8 +6,10 @@ module Players
         5
       elsif board.cells[0] != "X" && board.cells[0] != "O" && board.cells[8] != "X" && board.cells[8] != "O"
         small_array = ["1", "9"] 
+        small_array.sample 
       elsif board.cells[2] != "X" && board.cells[2] != "O" && board.cells[6] != "X" && board.cells[6] != "O"
         other_array = ["3", "7"]
+        other_array.sample 
       else 
         array = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
         array.sample 
