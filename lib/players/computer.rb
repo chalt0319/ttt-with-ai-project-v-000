04b70fe4d43958e6bbb381b0bbe27d7bc74c0e74
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-10 13:31:22 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -10,6 +10,8 @@ module Players
       elsif board.cells[2] != "X" && board.cells[2] != "O" && board.cells[6] != "X" && board.cells[6] != "O"
         other_array = ["3", "7"]
         other_array.sample 
+      elsif board.cells[0] == self.token && board.cells[2] == self.token
+        1 
       else 
         array = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
         array.sample 
